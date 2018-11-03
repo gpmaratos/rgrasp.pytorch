8b766fd097f0995559cf23bc09425c804f6808c4
G M <gmarat19@gmail.com>
2018-11-02 21:23:54 -0500

    fix Display constructor to account for data augmentation

--- evaluate.py
+++ evaluate.py
@@ -9,14 +9,17 @@ if __name__ == "__main__":
     parser.add_argument("params", type=str, help="path to weight file")
     parser.add_argument("bbone", type=str, help="backbone type")
     parser.add_argument("angles", type=int, help="number of angles or k")
+    parser.add_argument("-a", help="use image augmentation",
+    action="store_true")
     args = parser.parse_args()
     datap = args.dpath
-    weightp = args.params
     backb = args.bbone
+    weightp = args.params
     numa = args.angles
+    augs = args.a
 
-    #NEED TO ADD ARG CHECKS
+    #NEED SOME CHECKS ON THE ARGS
 
-    disp = Display(datap, backb, weightp, numa)
+    disp = Display(datap, backb, weightp, numa, augs)
     for i in range(100):
         disp.display(i)
