356e3ce44c19def17545eb06d674df68492a0cb2
G M <gmarat19@gmail.com>
2019-01-20 18:59:29 -0600

    change head to be a single object instead of dealing with classification and regression directly

--- models/general_rcnn.py
+++ models/general_rcnn.py
@@ -1,7 +1,6 @@
 from torch import nn
 from models.backbone import build_backbone
-from models.regression_head import build_regression_head
-from models.class_head import build_class_head
+from models.combined_head import build_head
 
 def build_RCNN(cfg):
     return GeneralRCNN(cfg)
@@ -16,14 +15,12 @@ class GeneralRCNN(nn.Module):
     def __init__(self, cfg):
         super(GeneralRCNN, self).__init__()
         backbone = build_backbone(cfg)
-        regression_head = build_regression_head(cfg)
-        class_head = build_class_head(cfg)
+        head = build_head(cfg)
+        self.head = head
         self.backbone = backbone
-        self.regression_head = regression_head
-        self.class_head = class_head
 
     def forward(self, img):
+        #loss calculation needs fixing (need to calculate final loss)
         features = self.backbone(img)
-        reg_scores, reg_loss = self.regression_head(features)
-        cls_scores, cls_loss = self.class_head(features)
+        #needs fixing for ghead
         return reg_scores, reg_loss, cls_scores, cls_loss
