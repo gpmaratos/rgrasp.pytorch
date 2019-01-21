ee786cf05c145f846696f218a3a24e8c291c7614
G M <gmarat19@gmail.com>
2019-01-20 18:56:19 -0600

    add code to check for cuda, put model on device, and start the training loop

--- engine/trainer.py
+++ engine/trainer.py
@@ -1,7 +1,27 @@
-def train(model, data_loader, optimizer, dev):
+import torch
+from torch import optim
+from data.CornellGrasp.dataloader import build_data_loader
+
+def train(model, cfg, start=1, end=100):
     """
     Training loop for Grasp RCNN.
     """
 
-    for bind, batch in enumerate(data_loader):
-        pass
+    print('Beginning Training')
+    if torch.cuda.is_available():
+        dev = torch.device('cuda:1')
+        print('Cuda detected, using GPU')
+    else:
+        dev = torch.dev('cpu')
+        print('No cuda detected, using CPU')
+
+    model = model.to(dev)
+    dl = build_data_loader(cfg)
+    optimizer = optim.SGD([
+        {'params': model.backbone.parameters(), 'lr':1e-5},
+        {'params': model.head.parameters()}
+        ],lr=1e-4, momentum=0.9
+    )
+
+    for bind, batch in enumerate(dl):
+        import pdb;pdb.set_trace()
