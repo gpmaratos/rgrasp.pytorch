7a9830395ea3a8b280cf0d9623eb78bc0de8d25d
G M <gmarat19@gmail.com>
2019-02-01 01:31:48 -0600

    move code to set torch device here, modify config slightly to add hyperparameters for loss function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import torch
 import argparse
 from models.general_rcnn import build_RCNN
 from utils.config import build_config
@@ -10,14 +11,25 @@ def main():
     args = parser.parse_args()
 
     dpath = args.train
-    b_size = 10
+    b_size = 15
+
+    print('Beginning Training')
+    if torch.cuda.is_available():
+        dev = torch.device('cuda:1')
+        print('Cuda detected, using GPU')
+    else:
+        dev = torch.dev('cpu')
+        print('No cuda detected, using CPU')
+
     nn_cfg = {
         'bbone_type':'resnet50',
         'num_ang':4,
         'reg_features':100,
         'cls_features':100,
+        'balance_factor':2,
+        'alpha':2.,
     }
-    cfg = build_config(dpath, b_size, nn_cfg)
+    cfg = build_config(dpath, dev, b_size, nn_cfg)
 
     if args.train:
         model = build_RCNN(cfg)
