6bcd8b902fbbbb25f7edfc154e4c14358b7fde32
G M <gmarat19@gmail.com>
2019-02-04 11:14:05 -0600

    calculate ray angle and length and draw

--- visualize/vis.py
+++ visualize/vis.py
@@ -1,4 +1,5 @@
 import os
+import math
 import skimage
 import numpy as np
 import matplotlib.pyplot as plt
@@ -37,12 +38,26 @@ class Visualizer(Dataset):
         bboxes = BoundingBoxList(f)
         return iarr, bboxes
 
+    def draw_tuple(self, draw, tup):
+#        import pdb;pdb.set_trace()
+        draw.ellipse((tup[0]-3, tup[1]-3, tup[0]+3, tup[1]+3),
+            fill=(0, 0, 200))
+        alpha = math.cos(math.radians(90)-tup[2])*10
+        beta = math.sin(math.radians(90)-tup[2])*10
+        x_ray = tup[0]+alpha
+        y_ray = tup[1]+beta
+        draw.line((tup[0], tup[1], x_ray, y_ray),
+            fill=(0, 200, 0))
+
     def show_ground_truth(self, iarr, bboxes):
         img = Image.fromarray(iarr)
         draw = ImageDraw.Draw(img)
-        for rec in bboxes.irecs:
+        for i in range(len(bboxes.ibboxes)):
+            rec = bboxes.irecs[i]
+            tup = bboxes.ibboxes[i]
             draw.line(rec[:2], fill=(200, 0, 0))
             draw.line(rec[1:], fill=(0, 0, 0))
+            self.draw_tuple(draw, tup)
         plt.imshow(img)
         plt.show()
 
