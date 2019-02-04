eee1e608b24d21e5928d0761bc68b5f320501ef2
G M <gmarat19@gmail.com>
2019-02-04 10:13:03 -0600

    add class for visualizing (still needs more work)

--- visualize/vis.py
+++ visualize/vis.py
@@ -1,4 +1,43 @@
-def build_visualizer():
-    return Visualizer()
+import os
+import skimage
+import numpy as np
+from PIL import Image, ImageDraw
+from torch.utils.data import Dataset
+from utils.bbox import BoundingBoxList
+from data.CornellGrasp.dataset import build_dataset
 
+def build_visualizer(cfg):
+    return Visualizer(cfg)
 
+class Visualizer(Dataset):
+    """
+    Visualizer. Used to show the annotations on an image, and visualize
+    the inference. It is a dataset object that gives images in a format
+    for visualizing, and later, to perform inference call prepare()
+    before passing into a network.
+    """
+
+    def __init__(self, dpath):
+        index = list(range(100, 950)) + list(range(1000, 1035))
+        del index[32]; del index[64]
+        index = np.array(index, dtype=int)
+        self.dpath = dpath
+        self.index = index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, idx):
+        ipref = os.path.join(self.dpath, "pcd%04d"%(self.index[idx]))
+        ipath = ipref + "r.png"
+        iarr = skimage.io.imread(ipath)
+        with open(ipref+"cpos.txt") as f:
+            f = f.read().split("\n")[:-1]
+        bboxes = BoundingBoxList(f)
+        return iarr, bboxes
+
+    def show_ground_truth(self, ind):
+        self.__getitem__(ind)
+
+    def prepare(self):
+        pass
