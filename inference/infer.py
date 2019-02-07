1efa6be7d66fa48aff81ad17fa952e14d8ec2e8c
G M <gmarat19@gmail.com>
2019-02-07 06:14:55 -0600

    add code to process tuple predictions

--- inference/infer.py
+++ inference/infer.py
@@ -1,4 +1,7 @@
+import math
 import torch
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 from torchvision.transforms import Normalize
 
 class Infer:
@@ -26,6 +29,17 @@ class Infer:
         self.pstride = pstride
         self.astride = astride
 
+    def draw_tuple(self, draw, tup):
+        #this is a copy from visualize
+        draw.ellipse((tup[0]-3, tup[1]-3, tup[0]+3, tup[1]+3),
+            fill=(0, 0, 200))
+        alpha = math.cos(math.radians(90)-tup[2])*10
+        beta = math.sin(math.radians(90)-tup[2])*10
+        x_ray = tup[0]+alpha
+        y_ray = tup[1]+beta
+        draw.line((tup[0], tup[1], x_ray, y_ray),
+            fill=(0, 200, 0))
+
     def __call__(self, model, inp):
         """
         Infer. Runs inference using the trained model. Runs a very slow
@@ -42,10 +56,15 @@ class Infer:
         for i in range(pred.shape[1]):
             for j in range(pred.shape[2]):
                 for k in range(3, pred.shape[3], self.num_ang):
+                    #change name of this variable so I do not exceed line
                     prediction = pred[0, i, j, k-3:k+1]
                     if prediction[3] > 0.7:
                         x = i*self.pstride + prediction[0].item()*self.pstride
                         y = j*self.pstride + prediction[1].item()*self.pstride
                         t = k/4*self.astride + prediction[2].item()*self.astride
                         preds.append((x, y, t))
-        return preds
+        img = Image.fromarray(inp)
+        draw = ImageDraw.Draw(img)
+        [self.draw_tuple(draw, tup) for tup in preds]
+        plt.imshow(img)
+        plt.show()
