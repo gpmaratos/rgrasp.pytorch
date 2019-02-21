10b168e44474acefc4aa5a8a01241dfdbc6a1f32
G M <gmarat19@gmail.com>
2019-02-21 09:26:48 -0600

    put placeholder for now to test data augmentation

--- engine/gt.py
+++ engine/gt.py
@@ -2,4 +2,4 @@ from data.CornellGrasp.dataset import build_dataset
 def gt(cfg):
     ds = build_dataset(cfg, train=False, aug=True)
     for example in ds:
-        import pdb;pbd.set_trace()
+        pass
