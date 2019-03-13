a2b2cac4518e52d665acda79e0070b6d8076b733
G M <gmarat19@gmail.com>
2019-03-13 15:01:49 -0500

    modify dataset to do an object wise split and subtract the background image

--- data_processing/dataset.py
+++ data_processing/dataset.py
@@ -1,14 +1,15 @@
 import os
 import torch
 import skimage
+import random
 import numpy as np
 from torch.utils.data import Dataset
-from utils.bbox import BoundingBoxList
+from structures.bbox import BoundingBoxList
 from torchvision.transforms import Normalize
-from utils.augment_image import build_augmenter
+from data_processing.augment_image import build_augmenter
 
-def build_dataset(cfg, train = True, aug = True):
-    return CornellDataset(cfg, train, aug)
+def build_dataset(cfg, train = True, aug = True, dset = 'train'):
+    return CornellDataset(cfg, train, aug, dset)
 
 class CornellDataset(Dataset):
     """
@@ -21,33 +22,67 @@ class CornellDataset(Dataset):
             example images
     """
 
-    def __init__(self, cfg, train, aug):
+    def __init__(self, cfg, train, aug, dset):
+        #first extract parameters from config and build image normalizer
         d_path = cfg.d_path
-        index = list(range(100, 950)) + list(range(1000, 1035))
-        del index[32]; del index[64]
-        index = np.array(index, dtype=int)
-        normalize = Normalize(
-            mean=[0.485, 0.456, 0.406] ,std=[0.229, 0.224, 0.225]
-        )
         if aug:
             x_dim = cfg.x_dim
             y_dim = cfg.y_dim
             augment = build_augmenter(x_dim, y_dim)
             self.augment = augment
+        normalize = Normalize(
+            mean=[0.485, 0.456, 0.406] ,std=[0.229, 0.224, 0.225]
+        )
 
+        #then create object lookup table and background mapping
+        with open(os.path.join(d_path, 'z.txt')) as f:
+            data = f.read().split('\n')[:-1]
+        lookup_dict = {}
+        bkg_dict = {}
+        for line in data:
+            p_line = line.split()
+            image_id, obj_num = int(p_line[0]), int(p_line[1])
+            background_img = int(p_line[3].split('_')[1])
+            if image_id >= 100 and image_id < 950 or\
+                image_id >= 1000 and image_id < 1035:
+                if not image_id in [135, 165]:
+                    if obj_num in lookup_dict:
+                        if not image_id in lookup_dict[obj_num]:
+                            lookup_dict[obj_num].append(image_id)
+                            bkg_dict[image_id] = background_img
+                    else:
+                        lookup_dict[obj_num] = [image_id]
+                        bkg_dict[image_id] = background_img
+
+        #then create train test split
+        random.seed(4211)
+        obj_ids = list(lookup_dict.keys())
+        random.shuffle(obj_ids)
+        split = int(0.8*len(obj_ids))
+        if dset == 'train':
+            index = np.array(obj_ids[:split])
+        if dset == 'test':
+            index = np.array(obj_ids[split:])
+
+        #finally create class members
         self.train = train
         self.d_path = d_path
-        self.index = index
         self.normalize = normalize
         self.aug = aug
+        self.lookup_dict = lookup_dict
+        self.bkg_dict = bkg_dict
+        self.index = index
 
     def __len__(self):
         return len(self.index)
 
-    def __getitem__(self, idx):
-        ipref = os.path.join(self.d_path, "pcd%04d"%(self.index[idx]))
+    def get_img(self, idx):
+        ipref = os.path.join(self.d_path, "pcd%04d"%(idx))
         ipath = ipref + "r.png"
+        iback = self.bkg_dict[idx]
+        bpath = os.path.join(self.d_path, "pcdb%04d"%(iback)+"r.png")
         iarr = skimage.io.imread(ipath)
+        iarr -= skimage.io.imread(bpath)
         with open(ipref+"cpos.txt") as f:
             f = f.read().split("\n")[:-1]
         bboxes = BoundingBoxList(f)
@@ -57,3 +92,9 @@ class CornellDataset(Dataset):
             iarr = torch.tensor(iarr).permute(2, 0, 1).float()
             iarr = self.normalize(iarr)
         return iarr, bboxes
+
+    def __getitem__(self, idx):
+        obj_index = self.index[idx]
+        indices = self.lookup_dict[obj_index]
+        imgs = [self.get_img(i) for i in indices]
+        return imgs
