1e95e5c88504b338c1e0fb77344eccd3e55e962b
G M <gmarat19@gmail.com>
2019-03-14 11:51:30 -0500

    modify to process list list tuple instead of list tuple

--- data_processing/dataloader.py
+++ data_processing/dataloader.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from data_processing.dataset import build_dataset
 
 def build_data_loader(cfg):
-    cdset = build_dataset(cfg)
+    cdset = build_dataset(cfg, aug=False)
     dsl = DataLoader(cdset, batch_size=cfg.b_size, collate_fn=collate_fn)
     return dsl
 
@@ -12,7 +12,7 @@ def collate_fn(slist):
     collate_fn. required to create a batch of examples from this dataset
     """
 
-    import pdb;pdb.set_trace()
-    x_img = torch.stack([e[0] for e in slist])
-    y_bboxes = [e[1] for e in slist]
+    tups = [item for lst in slist for item in lst]
+    x_img = torch.stack([e[0] for e in tups])
+    y_bboxes = [e[1] for e in tups]
     return (x_img, y_bboxes)
