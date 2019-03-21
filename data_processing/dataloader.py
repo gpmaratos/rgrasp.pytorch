47fa6b31dcb56c5a3a28eaae72873a6ab0519a80
G M <gmarat19@gmail.com>
2019-03-21 15:35:53 -0500

    change collate function name, return both normalized and permuted input images and not

--- data_processing/dataloader.py
+++ data_processing/dataloader.py
@@ -1,14 +1,16 @@
 import torch
+import numpy as np
 from torch.utils.data import DataLoader
 from data_processing.dataset import CornellDataset
 
-def collate_fn(slist):
+def collate(slist):
     """collate_fn. make batches from dataset outputs"""
 
     tups = [item for lst in slist for item in lst]
     x_img = torch.stack([e[0] for e in tups])
-    y_bboxes = [e[1] for e in tups]
-    return (x_img, y_bboxes)
+    x_img_orig = np.stack([e[1] for e in tups])
+    y_bboxes = [e[2] for e in tups]
+    return (x_img, x_img_orig, y_bboxes)
 
 def build_data_loader(batch_size, d_path, d_type):
     """build_data_loader. create wrapper for dataset for batch processing"""
