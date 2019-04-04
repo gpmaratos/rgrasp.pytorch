e12b7d37da60f7eeff1db7b9d77e6f1b41df0f89
G M <gmarat19@gmail.com>
2019-04-04 13:37:53 -0500

    add image cropping

--- data_processing/dataset.py
+++ data_processing/dataset.py
@@ -4,6 +4,7 @@ import skimage
 import random
 import torch
 import numpy as np
+from target_formatting.five_tuple import ConvertTuple
 from data_processing.augment_image import Augmenter
 from torch.utils.data import Dataset
 from torchvision.transforms import Normalize
@@ -15,6 +16,8 @@ class CornellDataset(Dataset):
     on the requirements specified in the pytorch documentation, and hopefully
     (future feature) be cropped and rotated randomly. Images 132 and 165 are
     ignored because their ground truth is not annotated properly.
+
+    Applied image cropping so that images are 320 320 crops of the center
 
     Arguments:
         d_path (string): path to the folder containing all the data. It needs
@@ -33,9 +36,9 @@ class CornellDataset(Dataset):
         normalize = Normalize(
             mean=[0.485, 0.456, 0.406] ,std=[0.229, 0.224, 0.225]
         )
-        img_size = (640, 480)
+        img_size = (320, 320)
         augmenter = Augmenter(img_size[0], img_size[1])
-#        gt_extract = GTExtractor(n_ang)
+        rec_formatter = ConvertTuple()
 
         #build object_lookup_table and background_lookup_table
         with open(os.path.join(d_path, 'z.txt')) as f:
@@ -75,13 +78,15 @@ class CornellDataset(Dataset):
         self.obj_lt = obj_lt
         self.bkg_lt = bkg_lt
         self.index = index
+        self.rec_formatter = rec_formatter
 
     def __len__(self):
         return len(self.index)
 
     def get_coord(self, f):
+        #image cropping offsets are subtracted here
         ln = f.split()
-        return (float(ln[0]), float(ln[1]))
+        return (float(ln[0]) - 120, float(ln[1]) - 100)
 
     def extract_img(self, img_id):
         """extract_img. Function that extracts a single image using its id"""
@@ -91,10 +96,13 @@ class CornellDataset(Dataset):
         img_path = img_pref + "r.png"
         np_img = skimage.io.imread(img_path)
 
-        #subtract background image
-        background = self.bkg_lt[img_id]
-        bkg_path = os.path.join(self.d_path, "pcdb%04d"%(background)+"r.png")
-        np_img -= skimage.io.imread(bkg_path)
+#        #subtract background image
+#        background = self.bkg_lt[img_id]
+#        bkg_path = os.path.join(self.d_path, "pcdb%04d"%(background)+"r.png")
+#        np_img -= skimage.io.imread(bkg_path)
+
+        #crop image to 320 by 320
+        np_img = skimage.util.crop(np_img, ( (100, 60), (120, 200), (0, 0)))
 
         #extract ground truth rectangles
         with open(img_pref+"cpos.txt") as f:
@@ -109,9 +117,9 @@ class CornellDataset(Dataset):
 
         #and augment image for training
         np_img_mod, gt_recs_mod = self.augmenter(np_img, gt_recs)
-#        gt_boxes_mod = self.gt_extract(gt_boxes_mod)
-        np_img_mod = torch.tensor(np_img_mod).permute(2, 0, 1).float()
-        np_img_mod = self.normalize(np_img_mod)
+#        gt_recs_mod = [self.rec_formatter(rec) for rec in gt_recs_mod]
+#        np_img_mod = torch.tensor(np_img_mod).permute(2, 0, 1).float()
+#        np_img_mod = self.normalize(np_img_mod)
 
         return np_img_mod, np_img, gt_recs_mod, gt_recs
 
