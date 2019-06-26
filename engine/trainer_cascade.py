42ffbe96da120cc7752f88f19a974b555b0e3573
G M <gmarat19@gmail.com>
2019-06-25 20:35:23 -0500

    fix the way metrics are printed

--- engine/trainer_cascade.py
+++ engine/trainer_cascade.py
@@ -9,49 +9,22 @@ from data_processing.dataloader import build_data_loader
 from utils.cuda_check import get_device
 from models.cascade_net import build_model
 
+
 def record(msg):
         print(msg)
         logging.info(msg)
 
-def calculate_metrics(measurement_dict, cls):
-        train_prec = measurement_dict['train_prec']
-        train_rec = measurement_dict['train_rec']
-        train_f1 = measurement_dict['train_f1']
-        val_prec = measurement_dict['val_prec']
-        val_rec = measurement_dict['val_rec']
-        val_f1 = measurement_dict['val_f1']
+def print_metrics(ds, loss, prec, rec, f1):
+    avg_loss = sum(loss)/len(loss)
+    avg_prec = sum(prec)/len(prec)
+    avg_rec = sum(rec)/len(rec)
+    avg_f1 = sum(f1)/len(f1)
 
-        avg_t_prec = np.array(train_prec)[:, cls].mean()
-        avg_t_rec = np.array(train_rec)[:, cls].mean()
-        avg_t_f1 = np.array(train_f1)[:, cls].mean()
-        avg_v_prec = np.array(val_prec)[:, cls].mean()
-        avg_v_rec = np.array(val_rec)[:, cls].mean()
-        avg_v_f1 = np.array(val_f1)[:, cls].mean()
-
-        results_dict = {
-            'avg_t_prec':avg_t_prec,
-            'avg_t_rec':avg_t_rec,
-            'avg_t_f1':avg_t_f1,
-            'avg_v_prec':avg_v_prec,
-            'avg_v_rec':avg_v_rec,
-            'avg_v_f1':avg_v_f1,
-        }
-        return results_dict
-
-def create_metrics_partial_message(results_dict, cls):
-        avg_t_prec = results_dict['avg_t_prec']
-        avg_t_rec = results_dict['avg_t_rec']
-        avg_t_f1 = results_dict['avg_t_f1']
-        avg_v_prec = results_dict['avg_v_prec']
-        avg_v_rec = results_dict['avg_v_rec']
-        avg_v_f1 = results_dict['avg_v_f1']
-
-        msg1 = "Results for cls: %d\n"%(cls)
-        msg2 = "Precision ~ Train: %f, Val: %f\n"%(avg_t_prec, avg_v_prec)
-        msg3 = "Recall ~ Train: %f, Val: %f\n"%(avg_t_rec, avg_v_rec)
-        msg4 = "F1 ~ Train: %f, Val: %f\n"%(avg_t_f1, avg_v_f1)
-
-        return msg1 + msg2 + msg3 + msg4
+    msg1 = 'Results for <%s>\n'%(ds)
+    msg2 = 'Cross Entropy Loss ~ %f\n'%(avg_loss)
+    msg3 = 'Precision and Recall ~ %f - %f\n'%(avg_prec, avg_rec)
+    msg4 = 'F1 Score ~ %f\n'%(avg_f1)
+    record(msg1+msg2+msg3+msg4)
 
 def train(d_path, w_path):
     """
@@ -60,7 +33,7 @@ def train(d_path, w_path):
     """
 
     #set script variables
-    epochs = 100
+    epochs = 500
     batch_size = 3
     device = get_device()
     #these still require number of angles for some reason
@@ -72,7 +45,7 @@ def train(d_path, w_path):
     model = build_model(device)
     opt = optim.SGD([
         {'params': model.detector.parameters()}
-        ],lr=1e-5, momentum=0.9, weight_decay=1e-4
+        ],lr=1e-3, momentum=0.9, weight_decay=1e-4
     )
 
     #run training loop
@@ -106,28 +79,11 @@ def train(d_path, w_path):
                 val_f1.append(result_dict['f1'])
         print("")
 
-        avg_t_loss = sum(train_loss)/len(dl_train)
-        avg_v_loss = sum(val_loss)/len(dl_val)
-        measurement_dict = {
-            'train_prec':train_prec,
-            'train_rec':train_rec,
-            'train_f1':train_f1,
-            'val_prec':val_prec,
-            'val_rec':val_rec,
-            'val_f1':val_f1,
-        }
-        neg_results = calculate_metrics(measurement_dict, 0)
-        pos_results = calculate_metrics(measurement_dict, 1)
-
-        msg3 = create_metrics_partial_message(neg_results, 0)
-        msg4 = create_metrics_partial_message(pos_results, 1)
-
         end = time.time()
         etime = (end-start)/60
-        msg1 = "Epoch: %d, Total Time: %.2fm\n"%(i+1, etime)
-        msg2 = "Cross Entropy Loss ~ Train: %f, Val: %f\n"%(avg_t_loss, avg_v_loss)
-        msg = msg1 + msg2 + msg3 + msg4
-        record(msg)
+        record('Epoch: %d, Total Time: %.2fm\n'%(i+1, etime))
+        print_metrics('train', train_loss, train_prec, train_rec, train_f1)
+        print_metrics('val', val_loss, val_prec, val_rec, val_f1)
 
     model = model.to(torch.device('cpu'))
     m_pref = 'model-' + str(datetime.date.today())
